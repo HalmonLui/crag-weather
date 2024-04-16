37bf86df3cf51b78952fbc07795987a608fccee4
Lui <luih@wit.edu>
2024-04-16 19:00:22 -0400

    Added weather icons

--- app/components/weathericon.tsx
+++ app/components/weathericon.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 
 // Icons
-import partlycloudyIcon from '../../public/weathericons/light_clouds.svg';
+import sunnyIcon from '../../public/weathericons/sunny.svg';
+import mainlyClearIcon from '../../public/weathericons/mainly_clear.svg';
+import partlycloudyIcon from '../../public/weathericons/partly_cloudy.svg';
+import cloudyIcon from '../../public/weathericons/cloudy.svg';
+import lightRainIcon from '../../public/weathericons/light_rain.svg';
+import heavyRainIcon from '../../public/weathericons/heavy_rain.svg';
+import lightSnowIcon from '../../public/weathericons/light_snow.svg';
+import heavySnowIcon from '../../public/weathericons/heavy_snow.svg';
+import thunderstormIcon from '../../public/weathericons/thunderstorm.svg';
 
 interface WeatherIconProps {
   weatherCode: number;
@@ -11,34 +19,34 @@ interface WeatherIconProps {
 const WeatherIcon: React.FC<WeatherIconProps> = ({ weatherCode }) => {
   // Define a mapping between weather codes and corresponding icon URLs or icon components
   const iconMapping: Record<number, string> = {
-    0: partlycloudyIcon, //clear sky
-    1: partlycloudyIcon, // mainly clear
+    0: sunnyIcon, //clear sky
+    1: mainlyClearIcon, // mainly clear
     2: partlycloudyIcon, // partly cloudy
-    3: partlycloudyIcon, // overcast
+    3: cloudyIcon, // overcast
     45: partlycloudyIcon, // fog
     48: partlycloudyIcon, // depositing rime fog
-    51: partlycloudyIcon, // light drizzle
-    53: partlycloudyIcon, // moderate drizzle
-    55: partlycloudyIcon, // dense drizzle
-    56: partlycloudyIcon, // light freezing drizzle
-    57: partlycloudyIcon, // dense freezing drizzle
-    61: partlycloudyIcon, // slight rain
-    63: partlycloudyIcon, // moderate rain
-    65: partlycloudyIcon, // heavy rain
-    66: partlycloudyIcon, // light frezing rain
-    67: partlycloudyIcon, // heavy freezing rain
-    71: partlycloudyIcon, // slight snow
-    73: partlycloudyIcon, // moderate snow
-    75: partlycloudyIcon, // heavy snow
+    51: lightRainIcon, // light drizzle
+    53: lightRainIcon, // moderate drizzle
+    55: lightRainIcon, // dense drizzle
+    56: lightRainIcon, // light freezing drizzle
+    57: lightRainIcon, // dense freezing drizzle
+    61: lightRainIcon, // slight rain
+    63: heavyRainIcon, // moderate rain
+    65: heavyRainIcon, // heavy rain
+    66: heavyRainIcon, // light frezing rain
+    67: heavyRainIcon, // heavy freezing rain
+    71: lightSnowIcon, // slight snow
+    73: lightSnowIcon, // moderate snow
+    75: heavySnowIcon, // heavy snow
     77: partlycloudyIcon, // snow grains
     80: partlycloudyIcon, // slight rain
     81: partlycloudyIcon, // moderate rain
     82: partlycloudyIcon, // violent rain
-    85: partlycloudyIcon, // slight snow
-    86: partlycloudyIcon, // heavy snow
-    95: partlycloudyIcon, // thunderstorm
-    96: partlycloudyIcon, // thunderstorm with slight hail
-    99: partlycloudyIcon, // thunderstorm with heavy hail
+    85: lightSnowIcon, // slight snow
+    86: heavySnowIcon, // heavy snow
+    95: thunderstormIcon, // thunderstorm
+    96: thunderstormIcon, // thunderstorm with slight hail
+    99: thunderstormIcon, // thunderstorm with heavy hail
   };
 
   // Get the icon URL based on the weatherCode
